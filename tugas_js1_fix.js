1460dfedfe20d8ef34c73d9865109682fa4fa60b
RizRev <rizkyrevanda@gmail.com>
2022-10-13 02:51:20 +0700

    saving tugas 1

---
+++ tugas_js1_fix.js
@@ -0,0 +1,36 @@
+// nomor 1
+printGanjilGenap(10)
+  let name = "Achmad Rizky Revanda"
+  var email = "rizkyrevanda@gmail.com"
+  const addres = {
+    location : "jl samudra 24b kamar no 10",
+    Province : "east java",
+    city : "Jember"
+  }
+  let isMerried = false
+  var hobbies = ["traveling","swimming","gaming"]
+  console.log (typeof(name));  
+  console.log(typeof(addres));
+  console.log(typeof(isMerried))
+
+  // nomor 2
+  function printGanjilGenap (angka){
+    for (let a = 1; a<=angka;a++){
+      let hasil = a%2==0 ? `${a} adalah bilangan genap` : `${a} adalah bilangan  ganjil`
+      console.log(hasil)
+    }
+  }
+  printGanjilGenap(10)
+
+  // nomor 3
+  function printTriangle (num){
+    let str = "";
+    for (let a = 1; a <= num; a++) {
+      for (let b = 1;b <=a;b++) {
+        str += a+" "
+      }
+      str += "\n"
+    }
+    return console.log(str)
+  }
+printTriangle(10)
