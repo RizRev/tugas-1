d60db4d9c20b92e5d7497dc02278a3785a98ab94
RizRev <rizkyrevanda@gmail.com>
2022-10-12 08:34:00 +0700

    modifikasi tugas 4

--- tugas4.js
+++ tugas4.js
@@ -15,17 +15,26 @@
 //     })
 // }
 // cekHariKerja('senin')
-//     .then(response => response)
+//     .then(response => console.log(response))
 //     .catch(response => console.log(response))
 
-// // tugas 2
-// function cekBulan(str) {
-//     month = ["January","February","Maret","April","Mei","Juni","Juli","Agustu","September","Oktober","November","Desember"]
-//     let cek = month.find((str) => {
-//         return str === month
-//     })
-//     console.log(cek)
-// }
+// then adalah perintah yang akan mengambil hasil dari resolve, sedangkan catch akan mengambil hasil dari reject. untuk try sendiri akan melakukan suatu fungsi kemudia akan menghasil sebuah hasil
+
+// tugas 2
+const cekBulan = (str2) => {
+    // str2 = ""
+    return new Promise((resolve, reject) => {
+        const month = ["January","February","Maret","April","Mei","Juni","Juli","Agustu","September","Oktober","November","Desember"]
+    var cek = month.find((str1) => {
+        return str1 === str2
+    })
+    if (cek) {
+        console.log("ada bulan")
+    } else {console.log("tidak ada bulan")}
+    })
+    // console.log(str2)
+}
+cekBulan("Mei")
 // const getMonth = (callback) => {
 //     setTimeout(() => {
 //         let error = false
@@ -42,32 +51,33 @@
 
 
 //tugas 3
-const cekdarah = (x) => new Promise((resolve, reject) => {
-    setTimeout(() => {
-        if (x === "a","b","o","ab") {
-            resolve('ini darah')
-        }  else {reject('ini bukan darah')}
-    },10000)
-})
-cekdarah("b")
-.then(response => console.log(response))
-.catch(response => console.log(response))
+// const cekdarah = (x) => new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         if (x === "a","b","o","ab") {
+//             resolve('ini darah')
+//         }  else {reject('ini bukan darah')}
+//     },10000)
+// })
+// cekdarah("b")
+// .then(response => console.log(response))
+// .catch(response => console.log(response))
 
-let perut = false
-const perlumakan = new Promise((resolve, reject) => {
-    if (perut) {
-        resolve('Sudah Kenyang')
-    } else {
-        reject('Masih Lapar')
-    }
-})
+// let perut = false
+// const perlumakan = new Promise((resolve, reject) => {
+//     if (perut) {
+//         resolve('Sudah Kenyang')
+//     } else {
+//         reject('Masih Lapar')
+//     }
+// })
 
-perlumakan
-.then(response => console.log(response))
-.catch(response => console.log(response))
+// perlumakan
+// .then(response => console.log(response))
+// .catch(response => console.log(response))
 
 
 // // tugas 4
+// import fetch from `cross-fetch`;
 // fetch('https://jsonplaceholder.typicode.com/users')
 // .then (response => response.json())
 // .then (json => console.log(json))
